4ca30fe43fc09b5baa197a8d32fff71c1de90431
bokyungB <bigdata.bk@gmail.com>
2022-04-05 18:20:56 +0900

    220405 one variable -> multi variable 실제치,예측치 plot

--- pandasTIL/model_plot.py
+++ pandasTIL/model_plot.py
@@ -6,21 +6,29 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import plast_only_cols as col_info
 
-df = pd.read_excel('../data/plast_only.xlsx', header=[0, 1, 2, 3])
-df.columns = col_info.columns
+pre_df = pd.read_excel('../data/plast_only.xlsx', header=[0, 1, 2, 3])
+pre_df.columns = col_info.columns
 x_axis_name, y_axis_name = '원료_Catalyst_금속촉매_TIPT (PIA 대비)','실험결과_분석결과_Coversion_Coversion'
 x_col, y_col = x_axis_name,y_axis_name
-df.dropna(subset=[x_col,y_col],inplace=True)
-X = df[x_col]
-Y = df[y_col]
+
+y_actual = pre_df[y_col]
+pre_df.dropna(subset=[x_col,y_col],inplace=True)
+
+df = pre_df.iloc[:,1:-1]
 
 model = LinearRegression()
-model.fit(X.values.reshape(-1,1),Y)
+# 한개의 독립변수가 아닌, 여러개의 독립변수 적용 
+model.fit(df,y_actual)
+y_predict = model.predict(df)
 
-y_range = model.predict(X.values.reshape(-1, 1))
+# 결과 확인해보기
+try:
+    test_df = df
+    test_df['y_actaul'] = y_actual
+    test_df['y_predict'] = y_predict
+    test_df.to_csv('model_result_check.csv')
+except:
+    pass
 
-fig = go.Figure([
-    go.Scatter(x=X, y=Y, name='Actual', mode='markers'),
-    go.Scatter(x=X, y=y_range, name='Prediction', mode='markers')
-])
+fig =  go.Figure([go.Scatter(x=y_predict, y=y_actual, mode = 'markers')])
 fig.show()
